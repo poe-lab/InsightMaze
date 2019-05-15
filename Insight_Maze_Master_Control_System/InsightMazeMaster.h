81c9681faf54bbd1269d31ec316c9d91ccf307b3
Gregory Suematsu <gsuematsu28@yahoo.com>
2019-05-14 21:45:00 -0700

    Editted master class and communication functions

--- Insight_Maze_Master_Control_System/InsightMazeMaster.h
+++ Insight_Maze_Master_Control_System/InsightMazeMaster.h
@@ -1,12 +1,4 @@
 
-const int NUM_MAIN_SENSORS = 4;
-const int NUM_MAIN_DOORS = 6;
-const int NUM_START_SENSORS = 4;
-const int NUM_START_DOORS = 6;
-const int NUM_END_SENSORS = 3;
-const int NUM_END_DOORS = 6;
-const int NUM_END_SYRINGE_PUMPS = 3;
-
 const int ID_VALS_OUT[] = {0, 51, 102, 153, 204, 255};
 
 const int ID_RES = 10;
